d266b25d61cd7ef1fb8eb3a2bfcf6c0aa96ac5b4
Sasha Koss <koss@nocorp.me>
2025-09-04 07:17:53 +0800

    Set up dev env

---
+++ .devcontainer/scripts/on-update.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# This script is when the container is updated.
+
+set -e
+
+# Pull git submodules
+if [ -d .git ]; then
+  git submodule update --recursive --init --remote
+fi
+
+# Trust all mise configs
+mise trust --yes --all
+if [ -d .git ]; then
+  git submodule foreach --recursive "mise trust"
+fi
+
+# Update mise
+mise self-update -y
+
+# Install stack
+mise install
+
+# Install dependencies
+
+# Rust
+if [ -f ./Cargo.lock ]; then
+  cargo build || echo "🟡 Cargo build failed, but that's ok"
+fi
+
+# Node.js
+if [ -f ./pnpm-lock.yaml ]; then
+  yes | pnpm install
+elif [ -f ./yarn.lock ]; then
+  yes | yarn install
+elif [ -f ./package-lock.json ]; then
+  yes | npm install
+fi
